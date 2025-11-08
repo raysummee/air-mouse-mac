f932df4057f7da64d8744167a36fa9fdf588b72e
Angshuman <angshuarin@gmail.com>
2025-11-09 02:10:38 +0530

    Initial Commit

---
+++ mouseBridgeHelper/mouseBridgeHelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  mouseBridgeHelperApp.swift
+//  mouseBridgeHelper
+//
+//  Created by Angshuman Barpujari on 11/9/25.
+//
+
+import SwiftUI
+
+@main
+struct mouseBridgeHelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
